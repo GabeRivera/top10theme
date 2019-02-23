eb70a2d9b13798bad605096471aa6d3e961159ab
Gabe Rivera <gabriel.f.rivera.10@gmail.com>
2019-02-23 18:03:15 -0500

    theming for buiness page

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,40 @@
 (function ($) {
   $(document).ready(function () {
+    const tabContainer = document.getElementsByClassName('content-tabs')[0];
+    const tabs = [...tabContainer.querySelectorAll('.content-tabs-item a')];
+    const contentItems = [...document.getElementsByClassName('content-tabs-cont-item')];
 
-    // put your code here
+    function tabClick(event) {
+      event.preventDefault();
+      const link = event.target;
+      if (!link.classList.contains('is-active')) {
+        const sectionName = link.href.split('#')[1];
+        tabs.forEach(tab => {
+          if (tab.href === link.href) {
+            tab.classList.add('is-active');
+          } else {
+            tab.classList.remove('is-active');
+          }
+        });
+        contentItems.forEach(item => {
+          if (item.id === sectionName) {
+            if (item.classList.contains('hidden')) {
+              item.classList.remove('hidden');
+            }
+          }
+          else {
+            item.classList.add('hidden');
+          }
+        });
+      }
+    }
 
+    if (tabs.length > 0) {
+      $('.content-tabs a').unbind();
+      tabs.forEach(tab => {
+        tab.addEventListener('click', tabClick);
+      });
+    }
+    
   }); 
 }(jQuery));
