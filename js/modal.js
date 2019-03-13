1f21eafd0a3ec4426b12bd193aef330b9a8e7c19
Gabe Rivera <gabriel.f.rivera.10@gmail.com>
2019-03-12 22:41:25 -0400

    business page fixed width

--- js/modal.js
+++ js/modal.js
@@ -2,13 +2,17 @@
     $(document).ready(function () {
     const modalBtns = document.getElementsByClassName('addReviewBtn');
     const form = document.getElementById('addReviewForm');
+    const body = document.getElementsByTagName('BODY')[0];
+    const btn = document.getElementById('closeFormBtn');
     function openModal(event) {
         form.classList.toggle('hidden');
+        body.classList.toggle('stuck');
     }
     if (modalBtns.length > 0) {
         for (let i = 0; i < modalBtns.length; i++) {
             modalBtns[i].addEventListener('click', openModal);
         }
+        btn.addEventListener('click', openModal)
     }
     
   
