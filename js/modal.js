e32f30b02cb7660e5c365bc0564b4539ff712d56
Cloudy-McCloudFace <Gabriel.rivera@corp.ovh.us>
2019-04-16 12:55:27 -0400

    style for modal form

--- js/modal.js
+++ js/modal.js
@@ -4,6 +4,25 @@
     const form = document.getElementById('addReviewForm');
     const body = document.getElementsByTagName('BODY')[0];
     const btn = document.getElementById('closeFormBtn');
+    const star = `&#9733;`;
+    const emptyStar = `&#9734;`;
+
+    const ratingSelect = document.getElementById('edit-rating');
+    const options = [...ratingSelect.querySelectorAll('option')];
+    options.forEach(option => {
+        const value = option.value;
+        const stars = [];
+        for (let i = 0; i < value; i++) {
+            stars.push(star);
+        }
+        for (let i = 0; i < 5 - value; i++) {
+            stars.push(emptyStar);
+        }
+        option.innerHTML = stars;
+    })
+
+
+
     function openModal(event) {
         form.classList.toggle('hidden');
         body.classList.toggle('stuck');
